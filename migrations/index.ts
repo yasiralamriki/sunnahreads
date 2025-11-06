80b2f4747f196a7ae412636263aaccbc38e99b98
ياسر الأمريكي <70416702+yasiralamriki@users.noreply.github.com>
2025-11-06 14:56:30 -0500

    breaking: remove migration files

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_20251106_102817 from './20251106_102817';
-
-export const migrations = [
-  {
-    up: migration_20251106_102817.up,
-    down: migration_20251106_102817.down,
-    name: '20251106_102817'
-  },
-];
+export const migrations = [];
